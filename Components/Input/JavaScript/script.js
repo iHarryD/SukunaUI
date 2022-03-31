7cc033210e6b028eb357206bf65298d938277b58
iHarryD <anuraagvr555@gmail.com>
2022-03-31 12:07:11 +0530

    revised for resubmission: added card variants

--- Components/Input/JavaScript/script.js
+++ Components/Input/JavaScript/script.js
@@ -1,16 +1,43 @@
-const allInputs = document.querySelectorAll(
-  ".component-section.--input .input"
-);
+const usernameEl = document.querySelector("#username");
+const passwordEl = document.querySelector("#password");
 
-function inputAnimation(element) {
-  window.addEventListener("click", (e) => {
-    if (element === document.activeElement) {
-      e.target.parentElement.classList.add("is-focused");
-    } else {
-      if (element.value) return;
-      element.parentElement.classList.remove("is-focused");
-    }
-  });
+function removeActiveClassFromBoth() {
+  usernameEl.classList.remove("is-focused");
+  passwordEl.classList.remove("is-focused");
 }
 
-allInputs.forEach((input) => inputAnimation(input));
+window.addEventListener("click", (e) => {
+  if (usernameEl === e.target || passwordEl === e.target) {
+    e.target.classList.add("is-focused");
+  }
+  if (!usernameEl.value && usernameEl !== document.activeElement) {
+    usernameEl.classList.remove("is-focused");
+  }
+  if (!passwordEl.value && passwordEl !== document.activeElement) {
+    passwordEl.classList.remove("is-focused");
+  }
+});
+
+function inputValidation(element, regEx, condition) {
+  const conditionCheck =
+    regEx.test(element.value) === condition && element.value.length >= 5;
+  if (conditionCheck) {
+    element.classList.remove("invalid-input");
+    element.classList.add("valid-input");
+  } else if (!conditionCheck) {
+    element.classList.remove("valid-input");
+    element.classList.add("invalid-input");
+  }
+  if (!element.value) {
+    element.classList.remove("invalid-input");
+    element.classList.remove("valid-input");
+  }
+  console.log(conditionCheck);
+}
+
+usernameEl.addEventListener("keyup", (e) =>
+  inputValidation(e.target, /\d/, false)
+);
+passwordEl.addEventListener("keyup", (e) =>
+  inputValidation(e.target, /\d/, true)
+);
